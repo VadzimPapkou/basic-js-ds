327a561988616b06a8fd4d964a294ae084124759
Vadim <playvarian@gmail.com>
2023-04-15 15:14:25 +0300

    feat: complete stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  stack = [];
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  push = Array.prototype.push.bind(this.stack)
+
+  pop = Array.prototype.pop.bind(this.stack)
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stack[this.stack.length - 1];
   }
 }
 
